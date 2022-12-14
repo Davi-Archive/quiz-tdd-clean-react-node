47433638aeee171b4b43e7df04ccff27ede086d7
d1av <davi4alves@gmail.com>
2022-12-14 15:18:04 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,25 @@
+import { MongoHelper } from "../helpers/mongo-helper"
+import { AccountMongoRepository } from './account'
+describe('Account Mongo Repository', () => {
+    beforeAll(async () => {
+        await MongoHelper.connect(process.env.MONGO_URL)
+    })
+
+    afterAll(() => {
+        await MongoHelper.disconnect()
+    })
+
+    test('Should return an account on success', async () => {
+        const sut = new AccountMongoRepository()
+        const account = sut.add({
+            name: 'any_name',
+            email: 'any_email@mail.com',
+            password: 'any_password'
+        })
+        expect(account).toBeTruthy()
+        expect(account.id).toBeTruthy()
+        expect(account.name).toEqual('any_name')
+        expect(account.email).toEqual('any_email@mail.com')
+        expect(account.password).toBe('any_password')
+    })
+})
